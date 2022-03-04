da16fc150852cc28db55e97ed9ca25201c744e95
mazad2 <karen.tihhomirov@gmail.com>
2022-03-04 08:47:05 +0200

    tund

--- 03_klassikomplekt/Main.java
+++ 03_klassikomplekt/Main.java
@@ -1,2 +1,22 @@
+// parem klõps -> new Java class -> main
+// main --> enter
+
 public class Main {
+    public static void main(String[] args) {
+        Human one = new Human("Mihkel",99,2.22,true);
+        Human two = new Human("AWHDB",3123,313.22,true);
+
+        System.out.println(one);
+        System.out.println(two);
+
+        one.sayHello();
+        two.sayHello();
+
+        // main meetod 1 class
+        // uus klass, kus on vähemalt 2 muutujat
+        //                  vähmalt 1 funktsioon
+        //              toString() funktsioon
+        // main meetodis on seda vähemalt 2 korda välja kutsutud
+        //          funktsiooni välja kutsutud
+    }
 }
